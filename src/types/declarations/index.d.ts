0b1979a0282e375e5b227ba7d573cfc4e7a03cec
vonsa <administratie@vonsa.nl>
2022-07-23 11:11:44 +0200

    sentry fixes

--- src/types/declarations/index.d.ts
+++ src/types/declarations/index.d.ts
@@ -6,9 +6,6 @@ interface ImportMetaEnv {
   VITE_CLIENT_ID: string
   VITE_GATEKEEPER_URL: string
   VITE_APOLLO_KEY: string
-  VITE_SENTRY_AUTH_TOKEN: string
-  VITE_SENTRY_ORG: string
-  VITE_SENTRY_PROJECT: string
   VITE_GTM_CONTAINER_ID: string
   VITE_VERSION: string
 }
